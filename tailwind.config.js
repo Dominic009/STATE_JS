c4434b921cbadb7fe40bcddc93e26de8518df0a8
Dominic009 <dpg11000@gmail.com>
2023-12-08 00:54:59 +0600

    3rd attempt

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+      fontFamily: {
+      // "class" : ["'Roboto Condensed', 'sans-serif'"],
+      "Lato" : ["'Lato', 'sans-serif'"],
+      "Sato" : ["'Sora', 'Poppins', 'Raleway', 'Oswald' ,'Work Sans'"],
+     },
+    extend: {},
+  },
+  plugins: [],
+}
+
+// ,  ,   "sans-serif
